264b1c736cc1522f36e184e3a7503d0248fd2340
A601-22 <tnk.dat.class@gmail.com>
2021-06-03 12:21:58 +0900

    ensyu

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,23 +4,50 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int a = 10;
+    int vx = 20;
+
+    public void Ensyu71()
     {
-        int a=10;
+        int vx = 5;
+        Debug.Log(vx);
+
+        vx = 10;
+        Debug.Log(vx);
+    }
+
+    public void Ensyu72()
+    {
+        Debug.Log(vx);
+    }
+
+    public void Ensyu74()
+    {
+        vx += 10;
+        Debug.Log(vx);
+    }
+
+    // Start is called before the first frame update
+    public void Proc()
+    {
+        
         float fw=3.14f;
         string str="こんにちは";
         bool bl=true;
 
-        Debug.Log(a);
+        Debug.Log(a+a);
         Debug.Log(fw);
         Debug.Log(str);
         Debug.Log(bl);
     }
 
+    public void Proc2()
+    {
+        Debug.Log(a);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
